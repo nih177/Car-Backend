8827984982608fd99538c50ddefd0677863fb0e2
unknown <borellinihanth2002@gmail.com>
2021-11-15 17:58:03 +0530

    assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -123,7 +123,8 @@
     </div>
 
     <!-- React JS -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
+    <script src='js/app.js' defer></script>
 
 </body>
 
